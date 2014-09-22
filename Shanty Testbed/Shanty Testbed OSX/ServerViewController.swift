6997142ae4c6fb1f57b2c820c379c8321c82661a
Jonathan Wight <jwight@mac.com>
2014-09-22 14:04:09 -0700

    Test bed: Weren’t actually opening a loopback port.

--- Shanty Testbed/Shanty Testbed OSX/ServerViewController.swift
+++ Shanty Testbed/Shanty Testbed OSX/ServerViewController.swift
@@ -40,7 +40,11 @@ class ServerViewController: NSViewController, STYListenerDelegate {
     }
 
     @IBAction func serve(sender:AnyObject?) {
-        self.server = STYListener(listeningAddress:STYAddress(anyAddress:port), netServiceDomain:self.domain, type:self.type, name:self.name)
+    
+        var address = self.loopback ? STYAddress(loopbackAddress:port) : STYAddress(anyAddress:port)
+    
+    
+        self.server = STYListener(listeningAddress:address, netServiceDomain:self.domain, type:self.type, name:self.name)
         self.server.delegate = self
         self.server.publishOnLocalhostOnly = self.loopback
         self.server.startListening() {
